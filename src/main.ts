c258f5e67dd362f972f8b233fe665d1c883e3e6c
Eduardo <eduardomoreno.eduardo@gmail.com>
2021-07-22 18:31:15 +0200

    add help command

--- src/main.ts
+++ src/main.ts
@@ -19,18 +19,37 @@ app.event('app_home_opened', async ({ message, say }) => {
 app.event('app_mention', async ({ event, context, client, say }) => {
   console.log(event.text)
   var cmd = event.text.substr(event.text.indexOf(">") + 2)
-  try {
-    await say({"blocks": [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `Thanks for the mention <@${event.user}>! I'm currently under development, but stay tuned! \n The command you sent was: \`${cmd}\``
-        },
-      }
+  
+  if (cmd.indexOf("help") != undefined) {
+    try {
+      await say({"blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": `*Commands:* \n\n \`help\` get command list \n \`<@U028PQ7M4KG> assign\` assign a sales rep to a Stripe user \n \`<@U028PQ7M4KG> unassign\` unassign a sales rep from a Stripe user`
+          },
+        }
     ]});
+    }
+    catch (error) {
+      console.error(error);
+    }
+  } else {
+      try {
+      await say({"blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": `Thanks for the mention <@${event.user}>! I'm currently under development, but stay tuned! \n The command you sent was: \`${cmd}\``
+          },
+        }
+      ]});
+    }
+    catch (error) {
+      console.error(error);
+    }
   }
-  catch (error) {
-    console.error(error);
-  }
+  
 });
