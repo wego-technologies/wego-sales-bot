6b193d7930bee1dc1374deb3e344ca8eff880e4a
Eduardo <eduardomoreno.eduardo@gmail.com>
2021-07-22 18:23:34 +0200

    Start working on utility

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,36 @@
-console.log('Hello TypeScript');
+require('dotenv').config()
+
+import { App } from '@slack/bolt';
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+(async () => {
+  await app.start({port: 3000});
+  console.log('⚡️ Bolt app is running!');
+})();
+
+app.event('app_home_opened', async ({ message, say }) => {
+  await say("Please use this bot in #gatego-sales");
+});
+
+app.event('app_mention', async ({ event, context, client, say }) => {
+  console.log(event.text)
+  var cmd = event.text.substr(event.text.indexOf(">") + 2)
+  try {
+    await say({"blocks": [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Thanks for the mention <@${event.user}>! I'm currently under development, but stay tuned! \n The command you sent was: \`${cmd}\``
+        },
+      }
+    ]});
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
