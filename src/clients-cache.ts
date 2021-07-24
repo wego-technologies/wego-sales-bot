532ddb01bb6fff8395717171d866ee358b71025b
Eduardo <eduardomoreno.eduardo@gmail.com>
2021-07-24 12:28:35 +0200

    Add cache logic

---
+++ src/clients-cache.ts
@@ -0,0 +1,21 @@
+import { Stripe } from "stripe";
+
+
+var clients : Stripe.Response<Stripe.ApiList<Stripe.Customer>>
+
+export async function updateCustomers(stripe: Stripe) {
+  console.log("Updating clients...");
+  
+  var clients = await stripe.customers.list({limit: 100});
+
+  console.log("Update complete!");
+  
+}
+
+export async function getCustomers(stripe : Stripe) {
+  if (clients == undefined) {
+    console.log("Empty clients list! Pre-filling with a few clients");
+    clients = await stripe.customers.list({limit:30});
+  }
+  return clients
+}
