fafb1ae31ef2e68179dca712b0784c90750383bb
Eduardo <eduardomoreno.eduardo@gmail.com>
2021-07-24 12:28:49 +0200

    Fix rounding issues on commission calculator

--- src/commsion-calc.ts
+++ src/commsion-calc.ts
@@ -1,9 +1,9 @@
-function calculateCommision(ammount : number) {
+function calculateCommision(ammount : number) : number {
   var percentCalc = ammount * 0.2
   if (percentCalc > 2000) {
     return 2000;
   } else {
-    return percentCalc.toFixed(2);
+    return Math.round((percentCalc + Number.EPSILON) * 100) / 100;
   }
 }
 
