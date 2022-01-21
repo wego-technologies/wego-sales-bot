24d02b6cac8a37eb873120a315cdc99583f8bf1c
Eduardo M <eduardomoreno.eduardo@gmail.com>
2022-01-21 10:16:33 +0100

    Reduce commission to 10%

--- src/commsion-calc.ts
+++ src/commsion-calc.ts
@@ -1,5 +1,5 @@
 function calculateCommision(ammount : number) : number {
-  var percentCalc = ammount * 0.2
+  var percentCalc = ammount * 0.1
   if (percentCalc > 2000) {
     return 2000;
   } else {
