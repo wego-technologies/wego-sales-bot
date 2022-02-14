8926d3d672f23470c46086860dfc6b2803aecb9e
Eduardo M <eduardomoreno.eduardo@gmail.com>
2022-02-14 10:40:41 +0100

    Exclude Stripe fee from comission

--- src/commsion-calc.ts
+++ src/commsion-calc.ts
@@ -1,5 +1,5 @@
 function calculateCommision(ammount : number) : number {
-  var percentCalc = ammount * 0.1
+  var percentCalc = (ammount -0.3) * 0.971 * 0.1
   if (percentCalc > 2000) {
     return 2000;
   } else {
